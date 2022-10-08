f443f99f7cfe1fd7e423f978071288b2998417b4
Karsten Pruehss <Karsten.Pruehss@xakiatech.com>
2022-10-08 15:07:52 +1100

    add dynamo get function helper

--- src/libs/dynamo.ts
+++ src/libs/dynamo.ts
@@ -1,5 +1,10 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { PutCommand, PutCommandInput } from '@aws-sdk/lib-dynamodb';
+import {
+  GetCommand,
+  GetCommandInput,
+  PutCommand,
+  PutCommandInput,
+} from '@aws-sdk/lib-dynamodb';
 
 const dynamoClient = new DynamoDBClient({});
 
@@ -14,5 +19,17 @@ export const dynamo = {
     await dynamoClient.send(command);
 
     return data;
+  },
+  get: async (id: string, tableName: string) => {
+    const params: GetCommandInput = {
+      TableName: tableName,
+      Key: {
+        id,
+      },
+    };
+    const command = new GetCommand(params);
+    const response = await dynamoClient.send(command);
+
+    return response.Item;
   },
 };
